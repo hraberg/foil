9ae1d248288ef0b4d5d525a81311ea296cb78d0e
Håkan Råberg <hakan.raberg@gmail.com>
2019-03-11 09:23:17 +0100

    Basic reading of stdin.

--- src/foil/main.clj
+++ src/foil/main.clj
@@ -1,4 +1,15 @@
 (ns foil.main
+  (:require [clojure.java.io :as io])
+  (:import java.io.PushbackReader)
   (:gen-class))
 
-(defn -main [& args])
+(defn- read-from [in]
+  (with-open [r (PushbackReader. (io/reader in))]
+    (binding [*read-eval* false]
+      (->> (repeatedly #(read r false ::eof))
+           (take-while #(not= ::eof %))
+           (vec)))))
+
+(defn -main [& args]
+  (doseq [x (read-from *in*)]
+    (prn x)))
