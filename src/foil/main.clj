13b43983961473eac71c04f8fd2dae0281ded4a5
Håkan Råberg <hakan.raberg@gmail.com>
2019-03-11 12:21:42 +0100

    Several more untested emitters.

--- src/foil/main.clj
+++ src/foil/main.clj
@@ -10,7 +10,12 @@
     (->> (repeatedly #(edn/read {:eof ::eof} r))
          (take-while #(not= ::eof %)))))
 
-(defn- emit-ns [[_ name & references :as form]]
+(def ^:private default-tag "void*")
+
+(defn form->tag [form]
+  (:tag (meta form) default-tag))
+
+(defn- emit-headers [[_ name & references :as form]]
   (doseq [[ref-type & lib-specs] references
           [lib :as lib-spec] lib-specs]
     (case ref-type
@@ -23,44 +28,153 @@
 (def ^:private default-indent "    ")
 (def ^:dynamic ^:private *indent* "")
 
+(def ^:private unary-op '{inc ++
+                          dec --
+                          not !
+                          ! !
+                          bit-not "~"})
+
+(def ^:private binary-op '{+ +
+                           - -
+                           / /
+                           * *
+                           mod %
+                           % %
+                           < <
+                           <= <=
+                           > >
+                           >= >=
+                           = ==
+                           == ==
+                           not= !=
+                           != !=
+                           and &&
+                           or ||
+                           bit-and &
+                           & &
+                           bit-or |
+                           | |
+                           bit-xor "^"
+                           bit-shift-left <<
+                           << <<
+                           bit-shift-right >>
+                           >> >>
+                           unsigned-bit-shift-right >>>
+                           >>> >>>})
+
 (defn- emit-application [[f & args :as form]]
-  (println (str *indent* f "(" (str/join ", " (map pr-str args)) ");")))
+  (cond
+    (contains? unary-op f)
+    (print (get unary-op f) (first args))
+
+    (contains? binary-op f)
+    (print (first args) " " (get binary-op f) " "  (second args))
+
+    :else
+    (print f "(" (str/join ", " (map pr-str args)) ")")))
 
 (defn- emit-return [[_ value :as from]]
-  (println (str *indent* "return " (pr-str value) ";")))
+  (print "return " (pr-str value)))
 
 (defn- emit-assignment [[_ var value :as from]]
-  (println (str *indent* var " = " (pr-str value) ";")))
+  (print var " = " (pr-str value)))
 
-(defn- emit-defn [[_ f args & body :as form]]
-  (println (str (:tag (meta args))
-                " "
-                f
-                (str "("
-                     (->> args
-                          (map #(str (:tag (meta %) "void*") " " %))
-                          (str/join ", "))
-                     ")")
-                " {"))
+(declare emit-block emit-expression)
+
+(defn- emit-while [[_ condition & body :as form]]
+  (print "while (")
+  (emit-expression condition)
+  (print ")")
+  (emit-block body))
+
+(defn- emit-bindings [bindings]
+  (doseq [[var binding] (partition 2 bindings)]
+    (println (form->tag var) " " var " " (pr-str binding) ";")
+    (println ";")))
+
+(defn- emit-let [[_ bindings & body :as form]]
+  (emit-bindings bindings)
+  (emit-block body))
+
+(defn- emit-conditional [[_ condition then else :as form]]
+  (emit-expression condition)
+  (print " ? ")
+  (emit-expression then)
+  (print " : ")
+  (emit-expression else))
+
+(def ^:dynamic ^:private *loop-state*)
+
+(defn- emit-loop-init [[_ bindings & body :as form]]
+  (emit-bindings bindings)
+  (binding [*loop-state* {:label (gensym "loop")
+                          :vars (mapv first bindings)}]
+    (println (:label *loop-state*) ":")
+    (emit-block body)))
+
+(defn- emit-goto [[_ & expressions :as form]]
+  (assert *loop-state* "Not in a loop.")
+  (doseq [[var expression] (map vector (:vars *loop-state*) expressions)]
+    (print var " = ")
+    (emit-expression expression)
+    (println ";"))
+  (println "goto "(:label *loop-state*) ";"))
+
+(defn- emit-expression [[op :as form]]
+  (case op
+    if (emit-conditional form)
+    (do (assert (and (symbol? op)
+                     (not (special-symbol? op)))
+                (str "Unsupported form: " (pr-str form)))
+        (when-let [tag (:tag (meta form))]
+          (print "(" tag ") "))
+        (emit-application form))))
+
+(defn- emit-expression-statement [[op :as form]]
+  (case op
+    return (emit-return form)
+    setq (emit-assignment form)
+    while (emit-while form)
+    let (emit-let form)
+    loop (emit-loop-init form)
+    recur (emit-goto form)
+    (emit-expression form)))
+
+(defn- emit-block [body]
+  (println "{")
   (binding [*indent* (str *indent* default-indent)]
     (doseq [x body]
       (assert (seq? x) (str "Unsupported form: " (pr-str x)))
-      (let [head (first x)]
-        (assert (and (symbol? head)
-                     (not (special-symbol? head)))
-                (str "Unsupported form: " (pr-str x)))
-        (case head
-          return (emit-return x)
-          setq (emit-assignment x)
-          (emit-application x)))))
+      (print *indent*)
+      (emit-expression-statement x)
+      (println ";")))
   (println "}"))
+
+(defn- emit-function [[_ f args & body :as form]]
+  (print (str (form->tag args)
+              " "
+              f
+              (str "("
+                   (->> args
+                        (map #(str (form->tag %) " " %))
+                        (str/join ", "))
+                   ") ")))
+  (emit-block body))
+
+
+(defn- emit-struct [[_ name fields :as form]]
+  (println "typedef struct {")
+  (doseq [field fields]
+    (print (form->tag field) " " field ";"))
+  (println "} " name ";"))
 
 (defn- emit-source [in out]
   (binding [*out* out]
     (doseq [[top-level :as form] (read-source in)]
       (case top-level
-        ns (emit-ns form)
-        defn (emit-defn form)))))
+        ns (emit-headers form)
+        defn (emit-function form)
+        defrecord (emit-struct form)))))
 
 (defn -main [& args]
   (emit-source *in* *out*))
