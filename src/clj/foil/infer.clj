fa2ccdd83f1b3b8d8bf7a1add468c9b8de70e839
Håkan Råberg <hakan.raberg@gmail.com>
2019-04-09 20:18:26 +0200

    More advanced inference, already quite messy.

--- src/clj/foil/infer.clj
+++ src/clj/foil/infer.clj
@@ -1,32 +1,50 @@
 (ns foil.infer
   (:require [clojure.string :as str]))
 
-(def replacements
-  {java.lang.Long 'long
+(def ^:private replacements
+  {java.lang.Byte 'int8_t
+   java.lang.Short 'int16_t
+   java.lang.Integer 'int32_t
+   java.lang.Long 'int64_t
+   java.lang.Character 'char
+   java.lang.Float 'float
    java.lang.Double 'double
-   java.lang.Boolean 'boolean
+   java.lang.Boolean 'bool
+   java.lang.String 'char*
    nil 'void})
 
-(defn logic-var? [x]
+(defn- logic-var? [x]
   (and (symbol? x)
        (str/starts-with? (str x) "?")))
 
-(defn unify [x y]
+(defn- unify [x y]
   (cond
     (logic-var? x)
-    y
+    (or y x)
 
     (logic-var? y)
-    x
+    (or x y)
 
     :else
     (do (assert (= x y)
                 (str x " != " y))
         x)))
 
+(defn- infer-binding-pairs [env binding-pairs]
+  (reduce
+   (fn [env [var binding]]
+     (let [t (infer env binding)
+           t (if-let [tag (:tag (meta var))]
+               (unify tag t)
+               t)]
+       (assoc env var t)))
+   env binding-pairs))
+
+(declare ^:dynamic *default-env*)
+
 (defn infer
   ([form]
-   (infer {'+ (infer {} '(fn [^?t x ^?t y] x))} form))
+   (infer *default-env* form))
   ([env form]
    (cond
      (seq? form)
@@ -39,14 +57,7 @@
                           (infer env else)))))
        do (last (mapv (partial infer env) (rest form)))
        let (let [[_ bindings & body] form
-                 env (reduce
-                      (fn [env [var binding]]
-                        (let [t (infer env binding)
-                              t (if-let [tag (:tag (meta var))]
-                                  (unify tag t)
-                                  t)]
-                          (assoc env var t)))
-                      env (partition 2 bindings))]
+                 env (infer-binding-pairs env (partition 2 bindings))]
              (infer env (cons 'do body)))
        fn (let [[_ args & body] form
                 env (reduce
@@ -54,19 +65,34 @@
                        (assoc env arg (or (:tag (meta arg))
                                           (gensym "?arg_"))))
                      env args)
-                return (infer env (cons 'do body))]
-            (with-meta (list return (mapv env args)) {:fn form}))
+                return (or (:tag (meta args)) (gensym "?return_"))
+                return (if (seq body)
+                         (unify return (infer env (cons 'do body)))
+                         return)]
+            (with-meta (list return '(*) (map env args)) {:fn form}))
        set! (let [[_ var value] form]
               (unify (infer env var) (infer env value)))
        (let [f (infer env (first form))
-             [return-t arg-ts] f
-             actual-arg-ts (mapv (partial infer env) (rest form))]
-         (assert (= (count arg-ts) (count actual-arg-ts)))
+             [return-t _ arg-ts] f
+             actual-args (rest form)]
+         (assert (= (count arg-ts) (count actual-args))
+                 (str "arity: "(count arg-ts) " != "(count actual-args)))
          (let [[_ args & body] (:fn (meta f))
-               actual-arg-ts (map unify arg-ts actual-arg-ts)
-               actual-return-t (infer (merge env (zipmap args actual-arg-ts))
-                                      (cons 'do body))]
-           (unify return-t actual-return-t))))
+               env (infer-binding-pairs env (map vector args actual-args))
+               bound-arg-ts (reduce
+                             (fn [acc [arg t]]
+                               (if (logic-var? t)
+                                 (let [candidate (infer env (unify arg t))]
+                                   (when-let [b (get acc t)]
+                                     (unify b candidate))
+                                   (assoc acc t candidate))
+                                 acc))
+                             {}
+                             (zipmap args arg-ts))
+               return-t (get bound-arg-ts return-t return-t)]
+           (if (seq body)
+             (unify return-t (infer env (cons 'do body)))
+             return-t))))
 
      (logic-var? form)
      (get env form form)
@@ -80,3 +106,8 @@
      (let [t (or (:tag (meta form))
                  (class form))]
        (get replacements t t)))))
+
+(def ^:dynamic *default-env*
+  {'+ (infer {} '(fn ^?t [^?t x ^?t y]))
+   '- (infer {} '(fn ^?t [^?t x ^?t y]))
+   '= (infer {} '(fn ^bool [^?t x ^?t y]))})
